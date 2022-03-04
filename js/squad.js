3fb7eca7a303e2c43689221834ffd4be4cd2c26d
Sam Lansford <sam.j.lansford@gmail.com>
2022-03-04 16:47:24 -0600

    Feat: adding boxes to the list of info

--- js/squad.js
+++ js/squad.js
@@ -1,34 +1,34 @@
 let data = [
     {
+        name: 'Mateo',
+        age: '30'
+    },
+    {
+        name: 'Sarah',
+        age: '32'
+    },
+    {
         name: 'John',
-        age:  '20'
+        age: '20'
     },
     {
-        name: 'Henry',
-        age:  '30'
-    },
-    {
-        name: 'Jay',
-        age:  '25'
-    },
-    {
-        name: 'David',
-        age:  '27'
+        name: 'Tim',
+        age: '27'
     },
     {
         name: 'Sam',
-        age:  '22'
+        age: '22'
     },
     {
-        name: 'Will',
-        age:  '28'
-    },
+        name: 'Joey',
+        age: '34'
+    }
 ];
 
-const info = document.querySelector("#info");
+const info = document.querySelector('#info');
 
-let details = data.map(function (item) {
-    return '<div>' + item.name + '<div>';
-})
+let details = data.map(function(item) {
+    return '<div>' + item.name + ' ' + 'is ' + item.age + ' years old' + '</div>';
+});
 
 info.innerHTML = details.join('\n');
