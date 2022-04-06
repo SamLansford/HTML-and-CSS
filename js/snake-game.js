d35847a2907f8d618892eeb18087712e61df44fe
Sam Lansford <sam.j.lansford@gmail.com>
2022-04-06 16:27:58 -0500

    Feat: adding move function

--- js/snake-game.js
+++ js/snake-game.js
@@ -7,6 +7,34 @@ class Snake {
         this.rotateX = 0;
         this.rotateY = 1;
     }
+
+    move() {
+        let newRect;
+        if (this.rotateX === 1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x + this.size,
+                Y: this.tail[this.tail.length - 1].y
+            }
+        } else if (this.rotateX === -1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x - this.size,
+                Y: this.tail[this.tail.length - 1].y
+            }
+        } else if (this.rotateY === 1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x,
+                Y: this.tail[this.tail.length - 1].y + this.size
+            }
+        } else if (this.rotateY === -1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x,
+                Y: this.tail[this.tail.length - 1].y - this.size
+            }
+        }
+
+        this.tail.shift()
+        this.tail.push(newRect)
+    }
 }
 
 let canvas = document.getElementById("canvas");
