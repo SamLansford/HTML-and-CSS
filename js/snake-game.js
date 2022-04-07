3185d6924c533f454d8b102d9ee4bebf3c1befb9
Sam Lansford <sam.j.lansford@gmail.com>
2022-04-07 16:19:48 -0500

    Feat: adding apple function

--- js/snake-game.js
+++ js/snake-game.js
@@ -37,6 +37,27 @@ class Snake {
     }
 }
 
+class Apple {
+    constructor() {
+        let isTouching;
+        while (true) {
+            isTouching = false;
+            this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
+            this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
+            for (let i = 0; i < snake.tail.length; i++) {
+                if (this.x === snake.tail[i].x && this.y === snake.tail[i].y) {
+                    isTouching = true
+                }
+            }
+            if (!isTouching) {
+                break;
+            }
+            this.color = "red"
+            this.size = snake.size;
+        }
+    }
+}
+
 let canvas = document.getElementById("canvas");
 
 let snake = new Snake();
