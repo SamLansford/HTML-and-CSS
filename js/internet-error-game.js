a282c63f041ce32df12a30ee485315313ad24eda
Sam Lansford <sam.j.lansford@gmail.com>
2022-04-17 23:24:56 -0500

    Feat: adding jump function

---
+++ js/internet-error-game.js
@@ -0,0 +1,10 @@
+let character = document.getElementById("character");
+let block = document.getElementById("block");
+let counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
+}
